a58331b2acf468898690c0162bd701ba66e51b61
wai <wai@wai-no-MacBook-Air.local>
2015-02-28 18:04:08 +0100

    offcanvas css 追加

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5d8ffa9eb7b92e64bf518c38b43c5201');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a91a1e9b8b496afdf69c5ef58c9d26c5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,41 +1,50 @@
 
   <nav class="navbar navbar-custom navbar-fixed-top top-nav-collapse sb-slide" role="navigation">
+      <div class="container-fluid">
 
-    <div class="container">
-        <!-- Left Control -->
-        <div class="sb-toggle-left navbar-left">
-          <i class="fa fa-bars"></i>
-        </div><!-- /.sb-control-left -->
 
-        <!-- Right Control -->
-        <div class="sb-toggle-right navbar-right">
-          <i class="fa fa-user"></i>
-        </div><!-- /.sb-control-right -->
 
-        <div class="navbar-header page-scroll">
-<!--             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                <i class="fa fa-bars"></i>
-            </button> -->
-            <a class="navbar-brand" href="/#page-top">
-                <img src="/wp-content/themes/dropit2015/assets/img/dropit_logo.png" alt="DROP iT">
-            </a>
+          <!-- Left Control -->
+          <div class="sb-toggle-left navbar-left">
+            <i class="offcanvas-btn fa fa-bars fa-2x">sdfsfsdfsfs</i>
+          </div><!-- /.sb-control-left -->
+          
+          <!-- Right Control -->
+          <div class="sb-toggle-right navbar-right">
+            <i class="offcanvas-btn fa fa-user fa-2x"></i>
+          </div><!-- /.sb-control-right -->
+
+
+
+        <div class="container">
+
+            <div class="navbar-header page-scroll">
+    <!--             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                    <i class="fa fa-bars"></i>
+                </button> -->
+                <a class="navbar-brand" href="/#page-top">
+                    <img src="/wp-content/themes/dropit2015/assets/img/dropit_logo.png" alt="DROP iT">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+
+                <ul class="nav navbar-nav nav-drop">
+                  <li class="menu-phone"><a href="tel:+33953009294"><span class="glyphicon glyphicon-earphone"></span> 09.53.00.92.94</a></li>
+                  <li class="menu-lang menu-lang-fr">
+                      <a href="/fr/"><img class="flag" src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/fr.png" alt="Français"></a> 
+                  </li>
+                  <li class="menu-lang menu-lang-ja">
+                      <a href="/ja/"><img class="flag" src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/ja.png" alt="日本語"></a>
+                  </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container -->
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-
-            <ul class="nav navbar-nav nav-drop">
-              <li class="menu-phone"><a href="tel:+33953009294"><span class="glyphicon glyphicon-earphone"></span> 09.53.00.92.94</a></li>
-              <li class="menu-lang menu-lang-fr">
-                  <a href="/fr/"><img class="flag" src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/fr.png" alt="Français"></a> 
-              </li>
-              <li class="menu-lang menu-lang-ja">
-                  <a href="/ja/"><img class="flag" src="/wp-content/plugins/sitepress-multilingual-cms/res/flags/ja.png" alt="日本語"></a>
-              </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
     </div>
-    <!-- /.container -->
+
 </nav>
 
